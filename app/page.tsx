9d90e4a00c26537fe141c25205d6fc6633fa082e
Hernando Abella <hernandoabella@gmail.com>
2024-07-28 23:14:43 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
 import Card from './components/LoveCard';
-
-const phrases = [
-  { title: 'Frase 1', description: 'Eres el semáforo a mi compilador.' },
-  { title: 'Frase 2', description: '¿Quieres ser mi framework y así te complemento?' },
-  { title: 'Frase 3', description: 'Nuestro amor es como un bucle infinito.' },
-];
+import { phrases } from './data/phrases';
 
 export default function Home() {
   return (

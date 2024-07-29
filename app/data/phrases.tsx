1a0351939f47c6fd0eac2926d876334e106657e7
Hernando Abella <hernandoabella@gmail.com>
2024-07-28 23:14:41 -0500

    Create phrases.tsx

---
+++ app/data/phrases.tsx
@@ -0,0 +1,9 @@
+export const phrases = [
+  { title: "Frase 1", description: "Eres el semáforo a mi compilador." },
+  {
+    title: "Frase 2",
+    description: "¿Quieres ser mi framework y así te complemento?",
+  },
+  { title: "Frase 3", description: "Nuestro amor es como un bucle infinito." },
+  // Puedes añadir más frases aquí
+];
